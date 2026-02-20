ec9a0610763b803d431ee960da8f5202410a1979
Veerman <Hasibuanfirman1404@gmail.com>
2026-02-20 23:46:43 +0700

    feat: add custom dataset with COCO format

---
+++ data/dataset.py
@@ -0,0 +1,36 @@
+import os
+import cv2
+import numpy as np
+from torch.utils.data import Dataset
+from pycocotools.coco import COCO
+
+class BrainSegmentationDataset(Dataset):
+    def __init__(self, img_dir, ann_file, transform=None):
+        self.img_dir = img_dir
+        self.coco = COCO(ann_file)
+        self.ids = list(self.coco.imgs.keys())
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx):
+        img_id = self.ids[idx]
+        img_info = self.coco.loadImgs(img_id)[0]
+        img_path = os.path.join(self.img_dir, img_info['file_name'])
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        ann_ids = self.coco.getAnnIds(imgIds=img_id)
+        anns = self.coco.loadAnns(ann_ids)
+        mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
+        for ann in anns:
+            mask = np.maximum(mask, self.coco.annToMask(ann))
+        
+        if self.transform:
+            augmented = self.transform(image=img, mask=mask)
+            img = augmented['image']
+            mask = augmented['mask']
+
+            mask = mask.unsqueeze(0).float()
+        
+        return img, mask
