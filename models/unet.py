22d345a57c76c43516c61e015ef68f103d29d3be
Veerman <Hasibuanfirman1404@gmail.com>
2026-02-21 00:35:54 +0700

    feat: add doubleconv block module

---
+++ models/unet.py
@@ -0,0 +1,17 @@
+import torch 
+import torch.nn as nn
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_c, out_c, 3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_c, out_c, 3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True)
+        )
+    
+    def forward(self, x):
+        return self.block(x)
