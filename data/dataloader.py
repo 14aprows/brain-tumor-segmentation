76781e144126c5750d980e7714dce22b7ba3736b
Veerman <Hasibuanfirman1404@gmail.com>
2026-02-21 15:23:48 +0700

    feat: add dataloaders

---
+++ data/dataloader.py
@@ -0,0 +1,14 @@
+import torch 
+from torch.utils.data import DataLoader
+
+from data.dataset import BrainSegmentationDataset
+from data.preprocessing import get_train_transforms, get_valid_transforms
+
+def get_dataloaders(train_img_dir, train_ann_file, valid_img_dir, valid_ann_file, batch_size=8, num_workers=4):
+    train_dataset = BrainSegmentationDataset(img_dir = train_img_dir, ann_file = train_ann_file, transform = get_train_transforms())
+    valid_dataset = BrainSegmentationDataset(img_dir = valid_img_dir, ann_file = valid_ann_file, transform = get_valid_transforms())
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
+
+    return train_loader, valid_loader
