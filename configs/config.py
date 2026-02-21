660c10794f7ffdd5e36e6c1617227902a40ed52d
Veerman <Hasibuanfirman1404@gmail.com>
2026-02-21 21:57:19 +0700

    refactor: update config

--- configs/config.py
+++ configs/config.py
@@ -1,6 +1,28 @@
-DATA_PATH="../brain-tumor-dataset"
-IMG_SIZE=256
-BATCH_SIZE=8
-LR=1e-4
-NUM_EPOCHS=50
-DEVICE="cuda" if __import__("torch").cuda.is_available() else "cpu"
+import torch
+import os
+
+class Config:
+    SEED = 42
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+    TRAIN_IMG_DIR = os.path.join(ROOT_DIR, "brain-tumor-dataset", "train")
+    TRAIN_ANN_FILE = os.path.join(TRAIN_IMG_DIR, "_annotations.coco.json")
+
+    VAL_IMG_DIR = os.path.join(ROOT_DIR, "brain-tumor-dataset", "valid")
+    VAL_ANN_FILE = os.path.join(VAL_IMG_DIR, "_annotations.coco.json")
+
+    BATCH_SIZE = 8
+    NUM_WORKERS = 4
+    IMAGE_SIZE = 256
+
+    IN_CHANNELS = 3
+    OUT_CHANNELS = 1
+
+    EPOCHS = 30
+    LR = 1e-4
+    WEIGHT_DECAY = 1e-5
+    T_MAX = 30
+
+    SAVE_DIR = "checkpoints"
