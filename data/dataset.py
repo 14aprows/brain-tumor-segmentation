ffdc7724009d99958f858e22b01c45d1272d2ea6
Veerman <Hasibuanfirman1404@gmail.com>
2026-02-21 21:59:03 +0700

    refactor: fix mask dimensions and ensure float type

--- data/dataset.py
+++ data/dataset.py
@@ -17,20 +17,27 @@ class BrainSegmentationDataset(Dataset):
     def __getitem__(self, idx):
         img_id = self.ids[idx]
         img_info = self.coco.loadImgs(img_id)[0]
-        img_path = os.path.join(self.img_dir, img_info['file_name'])
+
+        img_path = os.path.join(self.img_dir, img_info["file_name"])
         img = cv2.imread(img_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
+
         ann_ids = self.coco.getAnnIds(imgIds=img_id)
         anns = self.coco.loadAnns(ann_ids)
-        mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
+
         for ann in anns:
             mask = np.maximum(mask, self.coco.annToMask(ann))
-        
+
         if self.transform:
             augmented = self.transform(image=img, mask=mask)
-            img = augmented['image']
-            mask = augmented['mask']
+            img = augmented["image"]
+            mask = augmented["mask"]
 
-            mask = mask.unsqueeze(0).float()
+        if mask.ndim == 2:
+            mask = mask.unsqueeze(0)
         
+        mask = mask.float()
+
         return img, mask
